ffd6e0c29e92acfda0ee8b7d86fc1124a56bf883
maybMedia <braithmay@yahoo.com.au>
2024-02-09 11:31:08 +1100

    Updated styles

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    background: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
