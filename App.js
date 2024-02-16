b1dc316b7c1115ea010908f9c8dbe3f5713fe132
maybMedia <braithmay@yahoo.com.au>
2024-02-16 13:46:43 +1100

    Added components

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+
+const PlaceHolderImage = 'https://docs.expo.dev/static/images/tutorial/background-image.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={{uri: PlaceHolderImage}} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    background: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
 });
